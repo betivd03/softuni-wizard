46b943f974dc2c1d4710db7eb91cd48fbcc8031b
Beatris Borislavova <betivd03@abv.bg>
2021-06-25 20:06:06 +0300

    Add boundaries to movement

--- gameState.js
+++ gameState.js
@@ -4,6 +4,8 @@ function gameStateFactory() {
         wizard: {
             x: 100,
             y: 200,
+            width: 82,
+            height: 100,
             speed: 5,
         },
         keys: {},
